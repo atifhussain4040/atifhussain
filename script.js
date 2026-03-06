e2d280777f96a3fb00dba103902145881e037cb8
atifhussain0321 <atifhussain.org.com@gmail.com>
2026-03-06 15:56:07 +0530

    Add Responsiveness in Navbar & Made Scroll Nav

--- script.js
+++ script.js
@@ -10,11 +10,14 @@ function paddingFnc(elements){
         if (window.innerWidth < 500 && window.innerWidth > 0){
             padding = "0px 20px";
             elements[i].style.padding = padding;
-        } else if(window.innerWidth < 1000 && window.innerWidth > 500) {
+        } else if(window.innerWidth < 1400 && window.innerWidth > 500) {
             padding = "0px 20px";
             elements[i].style.padding = padding;
-        } else {
+        } else if(window.innerWidth < 2000 && window.innerWidth > 1100){
             padding = "0px " + window.innerWidth/5 + "px"; 
+            elements[i].style.padding = padding;
+        } else {
+            padding = "0px " + window.innerWidth/6 + "px"; 
             elements[i].style.padding = padding;
         }
         
@@ -23,3 +26,18 @@ function paddingFnc(elements){
 
 paddingFnc(paddingElements)
 window.addEventListener('resize', () => paddingFnc(paddingElements))
+
+// Navbar Change While Scrolling
+
+const scrollNav = document.querySelector(".Scroll-Nav")
+
+function navbarScroll(){
+    if (scrollY != 0){
+       scrollNav.style.display = "Flex" 
+       scrollNav.style.width = "50%"
+    } else {
+        scrollNav.style.display = "None"
+    }
+}
+window.addEventListener('scroll', navbarScroll)
+
